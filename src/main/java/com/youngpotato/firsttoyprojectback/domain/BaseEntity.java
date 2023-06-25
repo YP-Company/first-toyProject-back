cc38d83576853f9da062e078836aa53032e613bd
wujin <woojin97318@gmail.com>
2023-06-25 21:23:35 +0900

    feat: Auditing 설정 추가

---
+++ src/main/java/com/youngpotato/firsttoyprojectback/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.youngpotato.firsttoyprojectback.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "cre_time", nullable = false)
+    private LocalDateTime creTime;
+
+    @LastModifiedDate
+    @Column(name = "upd_time", nullable = false)
+    private LocalDateTime updTime;
+
+//    abstract public String name();
+}
