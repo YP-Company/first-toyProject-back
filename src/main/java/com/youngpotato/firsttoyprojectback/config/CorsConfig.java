31a2f1c7ac31f4c3fd377cd1b18bfb639ff4b7d0
wujin <woojin97318@gmail.com>
2023-09-17 11:46:28 +0900

    feat : CorsConfig 옵션 수정

--- src/main/java/com/youngpotato/firsttoyprojectback/config/CorsConfig.java
+++ src/main/java/com/youngpotato/firsttoyprojectback/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 js에서 처리할 수 있게 할지를 설정
-        config.addAllowedOrigin("*"); // 모든 IP의 응답을 허용
+        config.addAllowedOriginPattern("*"); // 모든 IP의 응답을 허용
         config.addAllowedHeader("*"); // 모든 header에 응답을 허옹
         config.addAllowedMethod("*"); // 모든 http method의 요청을 허용
 
